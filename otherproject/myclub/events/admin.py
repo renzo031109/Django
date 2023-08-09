34415b7fe495b0658a75a31fbbb8192282e82d45
renzo031109 <renzo031109@gmail.com>
2023-08-09 22:31:33 +0800

    modfy model

--- otherproject/myclub/events/admin.py
+++ otherproject/myclub/events/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Venue, MyClubUser, Event
 
-admin.site.register(Venue)
+#admin.site.register(Venue)
 admin.site.register(MyClubUser)
 admin.site.register(Event)
 
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name','address','phone')
--- otherproject/myclub/events/models.py
+++ otherproject/myclub/events/models.py
@@ -4,9 +4,9 @@ class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip Code', max_length=20)
-    phone = models.CharField('Contact Phone',max_length=20)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone',max_length=20, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email_address = models.EmailField('Email Address', blank=True)
 
     def __str__(self):
         return self.name
