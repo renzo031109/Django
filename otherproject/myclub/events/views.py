3c85da82b5b4acc45992e6873676d1c05f6f5187
renzo031109 <renzo031109@gmail.com>
2023-08-10 23:09:01 +0800

    added form add event

--- otherproject/myclub/events/views.py
+++ otherproject/myclub/events/views.py
@@ -4,12 +4,21 @@ from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event
 from .forms import VenueForm
+from django.http import HttpResponseRedirect
 
 
 def add_venue(request):
 	submitted = False
-	form = VenueForm
-	return render(request, 'events/add_venue.html', {'form': form})
+	if request.method == "POST":
+		form = VenueForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_venue?submitted=True')
+	else:
+		form = VenueForm
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, 'events/add_venue.html', {'form': form, 'submitted':submitted})
 
 def all_events(request):
 	event_list = Event.objects.all()
